9288b44948d44d23e22834d4038e18c6e06fc45d
Daniel Ortiz <daniel.ortiza.col@upb.edu.co>
2026-03-13 21:15:48 -0500

    Agregar recursos y estructura del proyecto Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.w8ly.fitness',
+  appName: 'W8ly',
+  webDir: 'dist'
+};
+
+export default config;
